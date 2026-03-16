24a95255dd5e54d91790d285dc06b97a4170da00
senandungarimbichh <senandungmarimby95@gmail.com>
2026-03-16 20:46:08 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp( 
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.pink,
+        appBar: AppBar(
+          title: Text(
+            "Dicee",
+            style: TextStyle(color: Colors.white),
+          ), 
+           backgroundColor: Colors.pink,
+        ),
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+    int leftDiceNumber = 1;
+    int rightDiceNumber = 1;
+
+void changeDiceFace() {
+      setState(() {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceFace();
+            },
+          child: Image.asset('images/dice$leftDiceNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceFace();
+            },
+          child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
+        ),
+        ],
+      ),
+    );
+  }
+}
